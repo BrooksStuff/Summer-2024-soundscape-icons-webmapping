b1770048807ee4651801dc5ef333dbd1fa5e94dd
BrooksStuff <josephbrooks509@gmail.com>
2024-06-04 13:15:07 -0500

    Assets and js

---
+++ assets/js/leaflet js test 2.js
@@ -0,0 +1,8 @@
+const map = L.map('mapid').setView([51.505, -0.09], 13);
+    
+    //tile location specification
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
